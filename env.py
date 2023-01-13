78746207d938a78c336c583329ccb43b9deb1fec
Hampus Strate <stratehampus02@gmail.com>
2023-01-13 10:01:41 +0000

    Add cloudinary and add three folders for templates, static and media. As well as an gitignore file and en env.py file.

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 import os
 os.environ["DATABASE_URL"] = "postgres://mvrakeew:KdvTj-xWJnm-5BS2MLoFp2Vyo4A_CGQP@snuffleupagus.db.elephantsql.com/mvrakeew"
-os.environ["SECRET_KEY"] = "strate75"
+os.environ["SECRET_KEY"] = "amelia02"
+os.environ["CLOUDINARY_URL"] = "cloudinary://134392323842427:zxiWWgoyu054IS24J_9-yIk0RrE@djefh6mbl"
